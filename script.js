6122d14689d3c52375086eb9fb8610c842491efe
jackthehash <ltwltw552@gmail.com>
2024-12-21 20:48:48 +0800

    added index.html and script.js and add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,8 @@
+function getComputerChoice() {
+    let x = Math.random();
+    if (x > 0 && x <= 0.33) {
+        return "rock";
+    } else if (x > 0.33 && x <= 0.66) {
+        return "paper";
+    } else return "scissors";
+}
